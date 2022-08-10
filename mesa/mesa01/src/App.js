19f0ce88418cdae8c71f4abdb620be680228edf0
Vinícius Marques <58312849+marqvin@users.noreply.github.com>
2022-08-09 23:04:18 -0300

    entendendo react

---
+++ mesa/mesa01/src/App.js
@@ -0,0 +1,13 @@
+// importa Componente Lista
+import Lista from "./componentes/Lista";
+
+function App() {
+
+  // Retorna Componente Lista em App
+  return (
+    <Lista />
+    
+  );
+}
+
+export default App;
---
+++ mesa/mesa01/src/componentes/Lista.js
@@ -0,0 +1,8 @@
+import Item from './Item';
+export default function Lista() {
+    return (
+        <ul>
+        <Item/>
+      </ul>
+    )
+}
